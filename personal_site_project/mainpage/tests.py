5ae14983441b6e257138cc304419e2746db4a08c
Jason Zoller <zoller.jason@gmail.com>
2017-03-11 15:14:12 -0600

    got first unittest to pass

--- personal_site_project/mainpage/tests.py
+++ personal_site_project/mainpage/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from mainpage.views import home_page
 
-class NotFailMathTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_love_the_leader(self):
-        self.assertEqual(2 + 2, 5)
+    def test_root_goes_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
