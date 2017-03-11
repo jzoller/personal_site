b93e448997f95b1586f1d7c70c1e336249d30025
Jason Zoller <zoller.jason@gmail.com>
2017-03-11 14:51:57 -0600

    integrated unittest library into functional_tests

--- personal_site_project/functional_tests.py
+++ personal_site_project/functional_tests.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class MainPageTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_main_page_has_correct_content(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Jaosn Zoller', self.browser.title)
+
+        # Next, test basic content
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
